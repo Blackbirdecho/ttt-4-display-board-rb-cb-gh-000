838e738ddb1f99da0dce8fcbd01ead4467543e6d
Khaled Ali Ahmed <s.echo.bird@gmail.com>
2019-04-15 19:28:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(card)
-  puts "#{card[0]} | #{card[1]} | #{card[2]}"
-  puts "---------------"
-    puts "#{card[3]} | #{card[4]} | #{card[5]}"
-      puts "------------"
-          puts "#{card[6]} | #{card[7]} | #{card[8]}"
+  puts " #{card[0]} | #{card[1]} | #{card[2]}"
+  puts "-----------"
+    puts " #{card[3]} | #{card[4]} | #{card[5]}"
+      puts "-----------"
+          puts " #{card[6]} | #{card[7]} | #{card[8]}"
         end
         board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
         display_board(board)
